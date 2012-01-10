fe3a37ca89c499ca883689ec0080e6da9e0d3fcb
robertofalzarano@gmail.com <robertofalzarano@gmail.com@4b0dcb0b-b6d4-a8da-5339-783aeab3a94a>
2012-01-10 13:48:44 +0000

    classe che ora estende la classe Service e non la classe IntentService

--- branches/developers/android/1.0/KidControllerLoginActivity/src/unisannio/kidController/Location/LocalizationService.java
+++ branches/developers/android/1.0/KidControllerLoginActivity/src/unisannio/kidController/Location/LocalizationService.java
@@ -11,29 +11,32 @@ import org.apache.http.client.methods.HttpGet;
 import org.apache.http.impl.client.DefaultHttpClient;
 
 import unisannio.kidController.Location.MyLocation.LocationResult;
+
 import android.R;
-import android.app.IntentService;
 import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.net.Uri;
+import android.os.IBinder;
 import android.os.PowerManager;
 import android.telephony.TelephonyManager;
 
-public class LocalizationService extends IntentService {
+public class LocalizationService extends Service {
 
-	private static final String  URI ="http://erny1790.no-ip.biz/KidC",//l'indirizzo dove è hostato il server
-												ACTION="Coordinate",
-												LAT="lat",
-												LON="lon",
-												IMEI="imei";
 	
+	private static final String  URI ="http://erny1790.no-ip.biz/KidC",//l'indirizzo dove è hostato il server
+			ACTION="Coordinate",
+			LAT="lat",
+			LON="lon",
+			IMEI="imei";
+
 	public final static int LOCATION_ID=12345;
 	public final static int START=0,
-										STOP=1;
+		STOP=1;
 	private MyLocation myL;
 	NotificationManager nm;
 	private int icon;
@@ -42,45 +45,32 @@ public class LocalizationService extends IntentService {
 	private CharSequence contentTitle;
 	private PowerManager.WakeLock wl;
 
-	
-	public LocalizationService() {
-		super("LocalizationService");
-		// TODO Auto-generated constructor stub
-		
-	}
-
-	
-
-	@Override
-	protected void onHandleIntent(Intent arg0) {
-		
-		int status = arg0.getIntExtra("startstop", START);
+	public int onStartCommand(Intent intent, int flags, int startId){
 		myL = new MyLocation();
 		PowerManager pm = (PowerManager) getSystemService(POWER_SERVICE);
 		wl = pm.newWakeLock(PowerManager.PARTIAL_WAKE_LOCK, "Location service lock");
 		wl.acquire();
-		//if(status == START){
-				nm = (NotificationManager) this.getSystemService(NOTIFICATION_SERVICE);
-			
-				icon =R.drawable.ic_menu_info_details;
-				tickerText = "Localization";
-	
-						
-				context = getApplicationContext();
-				contentTitle = "Location notify";
-			
-				myL.getLocation(this, result);
+		
+		nm = (NotificationManager) this.getSystemService(NOTIFICATION_SERVICE);
+		
+		icon =R.drawable.ic_menu_info_details;
+		tickerText = "Localization";
+
 				
-				while(true){
-					try {
-						Thread.sleep(600*1000);
-					} catch (InterruptedException e) {
-						// TODO Auto-generated catch block
-						e.printStackTrace();
-					}
-				}
-		//}	
+		context = getApplicationContext();
+		contentTitle = "Location notify";
+	
+		myL.getLocation(this, result);
+		
+		
+		return Service.START_STICKY;
 	}
+	@Override
+	public IBinder onBind(Intent arg0) {
+		
+		return null;
+	}
+
 	
 	public void onDestroy(){
 		wl.release();
@@ -88,67 +78,68 @@ public class LocalizationService extends IntentService {
 		super.onDestroy();
 	}
 	
-	//invio la richiesta al server e ne catturo il messaggio di risposta
-	private StatusLine sendRequest(String completeUri){
-
-		HttpClient httpclient = new DefaultHttpClient();
-		HttpResponse response;
-		
-		try {
-			response = httpclient.execute(new HttpGet(completeUri));
-			return response.getStatusLine();
-		} catch (ClientProtocolException e) {
-			e.printStackTrace();
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return null;
-		
-	}
 	
-	//recupero dei dati dall'intent e costruzione dell'indirizzo per la richiesta	
-	private String completeURI(double lat, double lon){
+	//invio la richiesta al server e ne catturo il messaggio di risposta
+		private StatusLine sendRequest(String completeUri){
 
-		TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
-		String imei=tm.getDeviceId();
-		return URI+"/"+ACTION+"?"+
-											LAT+"="+lat+"&"+
-											LON+"="+lon+"&"+
-											IMEI+"="+imei;
-	}
-
-	LocationResult result = new LocationResult() {
-		
-		@Override
-		public void gotLocation(Location location) {
-				if(location!=null){
-				double lat = location.getLatitude();
-				double lon = location.getLongitude();
-				long time = location.getTime();
-				float accuracy = location.getAccuracy();
-				String provider = location.getProvider();
-				
-				sendRequest(completeURI(lat,lon)); //invio posizione al server
-				
-				String ret = (lat+" latitudine\n"+
-									lon+" longitudine\n"+
-									"accuratezza: "+accuracy+"m\n"+
-									(new Time(time)).toLocaleString()+
-									"\nProvider: "+provider);
-				//Toast.makeText(getBaseContext(), ret, Toast.LENGTH_SHORT).show();
-				
-				
-				CharSequence contentText = ret;
-				Intent notificationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("www.google.it"));
-				PendingIntent contentIntent = PendingIntent.getActivity(getBaseContext(), 0, notificationIntent, 0);
-				
-				Notification notification =  new Notification(icon, tickerText, System.currentTimeMillis());
-				notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
-				
-				startForeground(LOCATION_ID, notification);
+			HttpClient httpclient = new DefaultHttpClient();
+			HttpResponse response;
+			
+			try {
+				response = httpclient.execute(new HttpGet(completeUri));
+				return response.getStatusLine();
+			} catch (ClientProtocolException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
 			}
+			return null;
 			
+		}
+		
+		//recupero dei dati dall'intent e costruzione dell'indirizzo per la richiesta	
+		private String completeURI(double lat, double lon){
+
+			TelephonyManager tm = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+			String imei=tm.getDeviceId();
+			return URI+"/"+ACTION+"?"+
+												LAT+"="+lat+"&"+
+												LON+"="+lon+"&"+
+												IMEI+"="+imei;
+		}
+
+		LocationResult result = new LocationResult() {
 			
-		}						
-	};
+			@Override
+			public void gotLocation(Location location) {
+					if(location!=null){
+					double lat = location.getLatitude();
+					double lon = location.getLongitude();
+					long time = location.getTime();
+					float accuracy = location.getAccuracy();
+					String provider = location.getProvider();
+					
+					sendRequest(completeURI(lat,lon)); //invio posizione al server
+					
+					String ret = (lat+" latitudine\n"+
+										lon+" longitudine\n"+
+										"accuratezza: "+accuracy+"m\n"+
+										(new Time(time)).toLocaleString()+
+										"\nProvider: "+provider);
+					//Toast.makeText(getBaseContext(), ret, Toast.LENGTH_SHORT).show();
+					
+					
+					CharSequence contentText = ret;
+					Intent notificationIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("www.google.it"));
+					PendingIntent contentIntent = PendingIntent.getActivity(getBaseContext(), 0, notificationIntent, 0);
+					
+					Notification notification =  new Notification(icon, tickerText, System.currentTimeMillis());
+					notification.setLatestEventInfo(context, contentTitle, contentText, contentIntent);
+					
+					startForeground(LOCATION_ID, notification);
+				}
+				
+				
+			}						
+		};
 }
